74103df60e7180eaa90bdc51bab943486d9ce9cb
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-02-14 21:22:44 +0300

    submit code

---
+++ controllers/FilesController.js
@@ -0,0 +1,29 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+async function postUpload() {
+    const userToken = req.get('X-token');
+
+    if (userToken) {
+      const userId = await redisClient.get(`auth_${userToken}`)
+      if (!userId) {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+      } else {
+        const content = res.body;
+        if (!content.name) {
+            return res.status(400).json({ error: 'Missing name' });
+        } else if (!content.type) {
+            return res.status(400).json({ error: 'Missing type' });
+        } else if (!content.data && content.type) {
+            return res.status(400).json({ error: 'Missing data' });
+        }
+        if (!content.parentId) {
+            return res.status(400).json({ error: 'Parent is not a folder' });
+        } 
+      }
+    }
+}
+
+module.exports = { postUpload };
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const Router = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 const stRoutes = Router();
 
@@ -11,5 +12,6 @@ stRoutes.post('/users', UsersController.postNew);
 stRoutes.get('/connect', AuthController.getConnect);
 stRoutes.get('/disconnect', AuthController.getDisconnect);
 stRoutes.get('/users/me', UsersController.getMe);
+stRoutes.post('/files', FilesController.postUpload);
 
 module.exports = stRoutes;
